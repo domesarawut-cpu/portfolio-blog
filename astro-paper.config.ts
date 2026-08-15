21817e5bbdc54e5bcda073b36f8e0d5370caa76e
domesarawut-cpu <domesarawut@gmail.com>
2026-08-15 12:46:55 -0400

    Update astro-paper.config.ts

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -24,7 +24,6 @@ export default defineAstroPaperConfig({
     showBackButton: true,
     editPost: {
       enabled: false,
-      url: "https://github.com/domesarawut-cpu/portfolio-blog/edit/main/",
     },
     search: "pagefind",
   },
